48517e04096bc56dbb2d88da10cb366242a2a205
Gustavo Flores <120065869+gusflores89@users.noreply.github.com>
2023-04-23 23:25:26 -0300

    Add files via upload

---
+++ intento3cac.js
@@ -0,0 +1,9 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+  const heroText = document.querySelector(".hero-text");
+  heroText.classList.add("animate__animated", "animate__fadeIn");
+});
+
+ 
+AOS.init();
+
